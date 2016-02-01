cdcca008ebbfee4b725c4133a56112f1fffa6e72
yimlu <luyiming2009@gmail.com>
2016-02-01 15:01:18 +0800

    Add platform specified root exception class

---
+++ Xemi.Core/XemiException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Xemi.Core
+{
+    public class XemiException:Exception
+    {
+         public XemiException()
+        { }
+
+        public XemiException(string message)
+            : base(message)
+        {
+
+        }
+
+        public XemiException(string messageFormat, params object[] args)
+            : base(string.Format(messageFormat, args))
+        {
+
+        }
+
+        public XemiException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
+        }
+
+        public XemiException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+    }
+}
