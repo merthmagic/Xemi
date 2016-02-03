f462bc182680dad5149c767bdfbc9c318362af3a
yimlu <luyiming2009@gmail.com>
2016-02-03 11:29:56 +0800

    Add methods to dependency registrar and resolver interfaces

--- Xemi.Core/Dependency/IDependencyRegistrar.cs
+++ Xemi.Core/Dependency/IDependencyRegistrar.cs
@@ -1,7 +1,23 @@
-﻿namespace Xemi.Core.Dependency
+﻿using System;
+
+namespace Xemi.Core.Dependency
 {
     public interface IDependencyRegistrar
     {
-         
+        void Register<T>(DependencyLifeStyle dependencyLifeStyle = DependencyLifeStyle.Singleton) 
+            where T : class;
+
+        void Register(Type type, DependencyLifeStyle dependencyLifeStyle = DependencyLifeStyle.Singleton);
+
+        void Register<TType, TImpl>(DependencyLifeStyle dependencyLifeStyle = DependencyLifeStyle.Singleton)
+            where TType : class
+            where TImpl : class;
+
+        void Register(Type type, Type typeImpl, DependencyLifeStyle dependencyLifeStyle = DependencyLifeStyle.Singleton);
+
+        //Register generic types is planed to be supported.
+        //But it may coz some problem if put this feature here, due to some 3rd party IoC components may not support this operation.
+        //So, this method may be removed from this interface and added in extension method in the future.
+        void RegisterGeneric(Type implementer);
     }
 }
--- Xemi.Core/Dependency/IDependencyResolver.cs
+++ Xemi.Core/Dependency/IDependencyResolver.cs
@@ -1,7 +1,17 @@
-﻿namespace Xemi.Core.Dependency
+﻿using System;
+
+namespace Xemi.Core.Dependency
 {
     public interface IDependencyResolver
     {
-         
+        T Resolve<T>();
+
+        object Resolve(Type type);
+
+        T[] ResolveAll<T>();
+
+        bool IsRegistered<T>();
+
+        bool IsRegistered(Type type);
     }
 }
