f6b4ad3ea033c29d8ca8c27ecaca1d53026b3524
yimlu <luyiming2009@gmail.com>
2016-02-03 10:44:02 +0800

    Finish ITypeFinder

--- Xemi.Core/Dependency/ITypeFinder.cs
+++ Xemi.Core/Dependency/ITypeFinder.cs
@@ -1,7 +1,28 @@
-﻿namespace Xemi.Core.Dependency
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Xemi.Core.Dependency
 {
     public interface ITypeFinder
     {
-         
+        IList<Assembly> GetAssemblies();
+
+        IEnumerable<Type> FindClassesOfType(Type assignTypeFrom, bool onlyConcreteClasses = true);
+
+        IEnumerable<Type> FindClassesOfType(Type assignTypeFrom, IEnumerable<Assembly> assemblies, bool onlyConcreteClasses = true);
+
+        IEnumerable<Type> FindClassesOfType<T>(bool onlyConcreteClasses = true);
+
+        IEnumerable<Type> FindClassesOfType<T>(IEnumerable<Assembly> assemblies, bool onlyConcreteClasses = true);
+
+        IEnumerable<Type> FindClassesOfType<T, TAssemblyAttribute>(bool onlyConcreteClasses = true) where TAssemblyAttribute : Attribute;
+
+        IEnumerable<Assembly> FindAssembliesWithAttribute<T>();
+
+        IEnumerable<Assembly> FindAssembliesWithAttribute<T>(IEnumerable<Assembly> assemblies);
+
+        IEnumerable<Assembly> FindAssembliesWithAttribute<T>(DirectoryInfo assemblyPath); 
     }
 }
