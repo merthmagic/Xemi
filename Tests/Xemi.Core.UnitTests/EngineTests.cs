ecb4c86f19b688ecb907d5aa02c150d887d451e7
yimlu <luyiming2009@gmail.com>
2016-02-03 16:46:07 +0800

    Test EngineContext initialize, this test failed, it should be fixed

---
+++ Tests/Xemi.Core.UnitTests/EngineTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using Xemi.Core.Configuration;
+
+namespace Xemi.Core.UnitTests
+{
+    [TestFixture]
+    public class EngineTests
+    {
+        private class StubConfigResolver : IConfigResolver
+        {
+            public string GetConfig(string key)
+            {
+                switch (key)
+                {
+                    case "Xemi:DependencyManagerType":
+                        {
+                            return "Xemi.Dependency.Autofac.DependencyManager";
+                        }
+                    default:
+                        {
+                            return null;
+                        }
+                }
+            }
+        }
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void InitializeEngine()
+        {
+            ConfigResolver.SetResolver(new StubConfigResolver());
+            EngineContext.Initialize();
+            var engine = EngineContext.Current;
+            Assert.IsNotNull(engine);
+        }
+    }
+
+
+}
