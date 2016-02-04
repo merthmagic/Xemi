de2893b18f3b3f61062274495ef9098ccc7e0edf
yimlu <luyiming2009@gmail.com>
2016-02-04 13:54:03 +0800

    Add caching

---
+++ src/Xemi.Core/Caching/NullCacheManager.cs
@@ -0,0 +1,35 @@
+﻿namespace Xemi.Core.Caching
+{
+    public class NullCacheManager : ICacheManager
+    {
+        public T Get<T>(string key)
+        {
+            return default(T);
+        }
+
+        public void Set(string key, object data, int cacheTime)
+        {
+
+        }
+
+        public bool IsSet(string key)
+        {
+            return false;
+        }
+
+        public void Remove(string key)
+        {
+
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+    }
+}
