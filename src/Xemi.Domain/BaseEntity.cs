77fba08ccf41fe63cecd9fa03448881ec5c99669
yimlu <luyiming2009@gmail.com>
2016-02-06 14:15:00 +0800

    Add entity base

---
+++ src/Xemi.Domain/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Xemi.Domain
+{
+    public abstract class BaseEntity<TKey> : IEntity<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+}
---
+++ src/Xemi.Domain/IEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Xemi.Domain
+{
+
+    public interface IEntity<out TKey>
+    {
+        TKey Id { get; }
+    }
+
+    public interface IEntity : IEntity<int>
+    {
+        
+    }
+}
